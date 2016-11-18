b6cc53a27f86e0400e27e9d5cd518803bb94289a
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-18 12:04:57 +0700

    Generalized Method

--- model/method/DElmdebug.php
+++ model/method/DElmdebug.php
@@ -60,39 +60,75 @@ class DElmdebug extends DBModel
         $this->data['raw'] = array('training' => $training, 'testing' => $testing);
 
         $this->metadata['total']['parameter'] = $feature;
-        $this->metadata['total']['data'] = count($training) + count($testing);
         $this->metadata['total']['training'] = count($training);
         $this->metadata['total']['testing'] = count($testing);
 
         $this->data['data']['training'] = $training;
         $this->data['data']['testing'] = $testing;
 
-/*        var_export($this->data['data']);
-        var_export($this->metadata['total']);
-        var_export($this->metadata['type']);*/
+        $this->generateClass();
     }
 
-    public function process($wjk, $bias, $normalization)
+    public function processLearning($feature, $training, $wjk = array(), $bias = array(), $normalization = array())
     {
+        $this->metadata['total']['parameter'] = $feature;
+        $this->metadata['total']['training'] = count($training);
+
+        $this->data['data']['training'] = $training;
+
+        $this->generateClass();
         $this->findMinMax($normalization);
-        $this->generateNormalization();
-        $this->generateDataMatrix();
-        $this->generateExpectedClass();
+        $this->generateNormalization('training');
+        $this->process($wjk, $bias);
+        $this->generateDataMatrix('training');
+        $this->generateExpectedClass('training');
+        $this->generateHInit('training');
+        $this->generateH('training');
+        $this->learn();
+        $this->generateYPredict('training');
+        $this->calculateAccuracy('training');
+    }
+
+    public function processTesting($testing)
+    {
+        $this->metadata['total']['testing'] = count($testing);
+        $this->data['data']['testing'] = $testing;
+        $this->generateNormalization('testing');
+        $this->generateDataMatrix('testing');
+        $this->generateExpectedClass('testing');
+        $this->generateHInit('testing');
+        $this->generateH('testing');
+        $this->generateYPredict('testing');
+        $this->calculateAccuracy('testing');
+    }
+
+    private function process($wjk, $bias)
+    {
         $this->generateElmProperties();
         $this->generateInitialInputWeight($wjk);
         $this->generateInputBias($bias);
-        $this->calculateELM();
     }
-
-    private function calculateELM()
+    
+    private function learn()
     {
-        $this->generateHInit();
-        $this->generateH();
         $this->generateHInvers();
         $this->generateHTopi();
         $this->generateBedaTopi();
-        $this->generateYPredict();
-        $this->calculateAccuracy();
+    }
+
+    public function getMape()
+    {
+        return $this->data['data']['mape'];
+    }
+
+    private function generateClass()
+    {
+        $uniqueClass = array();
+        foreach ($this->data['data']['training'] as $k1 => $v1)
+        {
+            array_push($uniqueClass, $this->data['data']['training'][$k1]['class']['actual']);
+        }
+        $this->metadata['class'] = array_values(array_unique($uniqueClass));
     }
 
     private function findMinMax($normalization = array())
@@ -123,58 +159,21 @@ class DElmdebug extends DBModel
         }
     }
 
-    private function generateNormalization()
+    private function generateNormalization($type)
     {
-        $this->data['normalization'] = $this->data['data'];
+        $this->data['normalization'][$type] = $this->data['data'][$type];
         $minmax = &$this->metadata['minmax'];
 
-        foreach ($this->metadata['type'] as $type)
+        foreach ($this->data['normalization'][$type] as $k1 => $v1)
         {
-            foreach ($this->data['normalization'][$type] as $k1 => $v1)
+            $tmpData = &$this->data['normalization'][$type][$k1]['data'];
+            foreach ($tmpData as $k2 => $v2)
             {
-                $tmpData = &$this->data['normalization'][$type][$k1]['data'];
-                foreach ($tmpData as $k2 => $v2)
-                {
-                    $tmpData[$k2] = ($v2 - $minmax['min']) / ($minmax['range']);
-                }
+                $tmpData[$k2] = ($v2 - $minmax['min']) / ($minmax['range']);
             }
         }
     }
-
-    private function generateDataMatrix()
-    {
-        foreach ($this->metadata['type'] as $type)
-        {
-            $tD = array();
-            foreach ($this->data['normalization'][$type] as $k1 => $v1)
-            {
-                array_push($tD, array());
-                $tD1 = &$tD[$k1];
-                foreach ($this->data['normalization'][$type][$k1]['data'] as $v3)
-                {
-                    array_push($tD1, $v3);
-                }
-            }
-            $this->data['elm']['data'][$type] = new Matrix($tD);
-        }
-    }
-
-    private function generateExpectedClass()
-    {
-        $uniqueClass = array();
-        $tD = array();
-        foreach ($this->data['normalization']['training'] as $k1 => $v1)
-        {
-            array_push($tD, array());
-            $tD1 = &$tD[$k1];
-            $class = $this->data['normalization']['training'][$k1]['class']['actual'];
-            array_push($tD1, $class);
-            array_push($uniqueClass, $class);
-        }
-        $this->data['elm']['data']['expectedClass'] = new Matrix($tD);
-        $this->metadata['class'] = array_values(array_unique($uniqueClass));
-    }
-
+    
     private function generateElmProperties()
     {
         $this->metadata['total']['layer']['input'] = $this->metadata['total']['parameter'];
@@ -196,7 +195,7 @@ class DElmdebug extends DBModel
                     array_push($tD1, ((float)mt_rand() / (float)mt_getrandmax()) - 0.5);
                 }
             }
-            $this->data['elm']['weight'] = new Matrix($tD);
+            $this->data['elm']['metadata']['weight'] = new Matrix($tD);
         }
         else
         {
@@ -210,7 +209,7 @@ class DElmdebug extends DBModel
                     array_push($tD1, $wjk[$i][$j]);
                 }
             }
-            $this->data['elm']['weight'] = new Matrix($tD);
+            $this->data['elm']['metadata']['weight'] = new Matrix($tD);
         }
     }
 
@@ -228,7 +227,7 @@ class DElmdebug extends DBModel
                     array_push($tD1, ((float)mt_rand() / (float)mt_getrandmax()));
                 }
             }
-            $this->data['elm']['bias'] = new Matrix($tD);
+            $this->data['elm']['metadata']['bias'] = new Matrix($tD);
         }
         else
         {
@@ -242,18 +241,46 @@ class DElmdebug extends DBModel
                     array_push($tD1, $bias[$i][$j]);
                 }
             }
-            $this->data['elm']['bias'] = new Matrix($tD);
+            $this->data['elm']['metadata']['bias'] = new Matrix($tD);
         }
     }
 
-    private function generateHInit()
+    private function generateDataMatrix($type)
     {
-        $this->data['elm']['h_init'] = ($this->data['elm']['data']['training'])->multiplyMatrix(($this->data['elm']['weight'])->transpose());
+        $tD = array();
+        foreach ($this->data['normalization'][$type] as $k1 => $v1)
+        {
+            array_push($tD, array());
+            $tD1 = &$tD[$k1];
+            foreach ($this->data['normalization'][$type][$k1]['data'] as $v3)
+            {
+                array_push($tD1, $v3);
+            }
+        }
+        $this->data['elm']['data'][$type]['data'] = new Matrix($tD);
     }
 
-    private function generateH()
+    private function generateExpectedClass($type)
     {
-        $this->data['elm']['h'] = ($this->data['elm']['h_init'])->map(function ($element)
+        $tD = array();
+        foreach ($this->data['normalization'][$type] as $k1 => $v1)
+        {
+            array_push($tD, array());
+            $tD1 = &$tD[$k1];
+            $class = $this->data['normalization'][$type][$k1]['class']['actual'];
+            array_push($tD1, $class);
+        }
+        $this->data['elm']['data'][$type]['class']['expected'] = new Matrix($tD);
+    }
+    
+    private function generateHInit($type)
+    {
+        $this->data['elm']['data'][$type]['h_init'] = ($this->data['elm']['data'][$type]['data'])->multiplyMatrix(($this->data['elm']['metadata']['weight'])->transpose());
+    }
+
+    private function generateH($type)
+    {
+        $this->data['elm']['data'][$type]['h'] = ($this->data['elm']['data'][$type]['h_init'])->map(function ($element)
         {
             return (1.0 / (1.0 + exp(-$element)));
         });
@@ -261,31 +288,31 @@ class DElmdebug extends DBModel
 
     private function generateHInvers()
     {
-        $this->data['elm']['h_inverse'] = ((($this->data['elm']['h'])->transpose())->multiplyMatrix($this->data['elm']['h']))->inverse();
+        $this->data['elm']['metadata']['h_inverse'] = ((($this->data['elm']['data']['training']['h'])->transpose())->multiplyMatrix($this->data['elm']['data']['training']['h']))->inverse();
     }
 
     private function generateHTopi()
     {
-        $this->data['elm']['h_topi'] = ($this->data['elm']['h_inverse'])->multiplyMatrix(($this->data['elm']['h'])->transpose());
+        $this->data['elm']['metadata']['h_topi'] = ($this->data['elm']['metadata']['h_inverse'])->multiplyMatrix(($this->data['elm']['data']['training']['h'])->transpose());
     }
 
     private function generateBedaTopi()
     {
-        $this->data['elm']['beda_topi'] = ($this->data['elm']['h_topi'])->multiplyMatrix($this->data['elm']['data']['expectedClass']);
+        $this->data['elm']['metadata']['beda_topi'] = ($this->data['elm']['metadata']['h_topi'])->multiplyMatrix($this->data['elm']['data']['training']['class']['expected']);
     }
 
-    private function generateYPredict()
+    private function generateYPredict($type)
     {
-        $this->data['elm']['y_predict'] = ($this->data['elm']['h'])->multiplyMatrix($this->data['elm']['beda_topi']);
-        $this->data['y_predict'] = &$this->data['elm']['y_predict']->toArray();
+        $this->data['elm']['metadata'][$type]['y_predict'] = ($this->data['elm']['data'][$type]['h'])->multiplyMatrix($this->data['elm']['metadata']['beda_topi']);
+        $this->metadata['y_predict'][$type] = &$this->data['elm']['metadata'][$type]['y_predict']->toArray();
     }
 
-    private function calculateAccuracy()
+    private function calculateAccuracy($type)
     {
         $mape = 0.0;
-        $training = &$this->data['data']['training'];
-        $predict = &$this->data['y_predict'];
-        $totalData = $this->metadata['total']['training'];
+        $data = &$this->data['data'][$type];
+        $predict = &$this->metadata['y_predict'][$type];
+        $totalData = $this->metadata['total'][$type];
         for($i = -1; ++$i < $totalData;)
         {
             $value = PHP_INT_MAX;
@@ -293,15 +320,16 @@ class DElmdebug extends DBModel
             foreach ($this->metadata['class'] as $class)
             {
                 $tmpValue = abs($predict[$i][0] - $class);
+
                 if($tmpValue < $value)
                 {
                     $index = $class;
                     $value = $tmpValue;
                 }
             }
-            $training[$i]['class']['actual'] = $index;
-            $mape += abs(($predict[$i][0] - $training[$i]['class']['actual'])/$training[$i]['class']['actual']);
+            $data[$i]['class']['actual'] = $index;
+            $mape += abs(($predict[$i][0] - $data[$i]['class']['actual'])/$data[$i]['class']['actual']);
         }
-        $this->data['mape'] = $mape / $totalData * 100.0;
+        $this->data['data']['mape'][$type] = $mape / $totalData * 100.0;
     }
 }
