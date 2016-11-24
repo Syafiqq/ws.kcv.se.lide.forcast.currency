fd62352331fa2432e2a021b7a65f685afbbc3b3d
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-25 00:05:29 +0700

    Completing Testing Function

--- controller/method/CElm.php
+++ controller/method/CElm.php
@@ -12,7 +12,6 @@ use model\Currency as DBCurrency;
 use model\Exchange as DBExchange;
 use model\logger\Logger;
 use model\method\DElm_tmp;
-use model\method\DElmdebug;
 use model\method\MElm;
 use Pux;
 
@@ -70,94 +69,152 @@ class CElm extends Pux\Controller
         }
     }
 
-    public function test()
-    {
-        $training = array(
-            array('data' => array(1, 1, 1), 'class' => array('actual' => 1, 'expected' => 1)),
-            array('data' => array(1, 0, 1), 'class' => array('actual' => 1, 'expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('actual' => 1, 'expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('actual' => 2, 'expected' => 2)),
-            array('data' => array(0, 1, 0), 'class' => array('actual' => 2, 'expected' => 2)),
-            array('data' => array(0, 0, 0), 'class' => array('actual' => 2, 'expected' => 2)),
-            array('data' => array(0, 1, 0), 'class' => array('actual' => 3, 'expected' => 3)),
-            array('data' => array(1, 1, 0), 'class' => array('actual' => 3, 'expected' => 3)),
-            array('data' => array(0, 0, 0), 'class' => array('actual' => 3, 'expected' => 3))
-        );
-        $testing = array(
-            array('data' => array(1, 0, 1), 'class' => array('actual' => -1, 'expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('actual' => -1, 'expected' => 1)),
-            array('data' => array(0, 1, 0), 'class' => array('actual' => -1, 'expected' => 3))
-        );
+    /*    public function test()
+        {
+            $training = array(
+                array('data' => array(1, 1, 1), 'class' => array('actual' => 1, 'expected' => 1)),
+                array('data' => array(1, 0, 1), 'class' => array('actual' => 1, 'expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('actual' => 1, 'expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('actual' => 2, 'expected' => 2)),
+                array('data' => array(0, 1, 0), 'class' => array('actual' => 2, 'expected' => 2)),
+                array('data' => array(0, 0, 0), 'class' => array('actual' => 2, 'expected' => 2)),
+                array('data' => array(0, 1, 0), 'class' => array('actual' => 3, 'expected' => 3)),
+                array('data' => array(1, 1, 0), 'class' => array('actual' => 3, 'expected' => 3)),
+                array('data' => array(0, 0, 0), 'class' => array('actual' => 3, 'expected' => 3))
+            );
+            $testing = array(
+                array('data' => array(1, 0, 1), 'class' => array('actual' => -1, 'expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('actual' => -1, 'expected' => 1)),
+                array('data' => array(0, 1, 0), 'class' => array('actual' => -1, 'expected' => 3))
+            );
 
-        $feature = 3;
+            $feature = 3;
 
-        $wjk = array(
-            array(-0.4,  0.2,  0.1),
-            array(-0.2,  0.0,  0.4),
-            array(-0.3,  0.3, -0.1)
-        );
+            $wjk = array(
+                array(-0.4,  0.2,  0.1),
+                array(-0.2,  0.0,  0.4),
+                array(-0.3,  0.3, -0.1)
+            );
 
-        $bias = array(
-            array(0.1),
-            array(0.4),
-            array(0.3)
-        );
+            $bias = array(
+                array(0.1),
+                array(0.4),
+                array(0.3)
+            );
 
-        $normalization = array('min' => 0, 'max' => 1);
+            $normalization = array('min' => 0, 'max' => 1);
 
-        $elmMod = DElmdebug::getInstance();
+            $elmMod = DElmdebug::getInstance();
 
-        $elmMod->processLearning($feature, $training, $wjk, $bias, $normalization);
-        $elmMod->processTesting($testing);
-        var_dump($elmMod->getMape());
+            $elmMod->processLearning($feature, $training, $wjk, $bias, $normalization);
+            $elmMod->processTesting($testing);
+            var_dump($elmMod->getMape());
 
-    }
+        }
 
-    public function mfinal()
-    {
-        $elm = MElm::getInstance();
-        $feature = 3;
-        $weight = MElm::generateWeight($feature);
-        $bias = MElm::generateBias($feature);
-        $minMax = MElm::generateNormalizationBound(array(0, 1), 0);
+        public function mfinal()
+        {
+            $elm = MElm::getInstance();
+            $feature = 3;
+            $weight = MElm::generateWeight($feature);
+            $bias = MElm::generateBias($feature);
+            $minMax = MElm::generateNormalizationBound(array(0, 1), 0);
 
-        $training = array(
-            array('data' => array(1, 1, 1), 'class' => array('expected' => 1)),
-            array('data' => array(1, 0, 1), 'class' => array('expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('expected' => 2)),
-            array('data' => array(0, 1, 0), 'class' => array('expected' => 2)),
-            array('data' => array(0, 0, 0), 'class' => array('expected' => 2)),
-            array('data' => array(0, 1, 0), 'class' => array('expected' => 3)),
-            array('data' => array(1, 1, 0), 'class' => array('expected' => 3)),
-            array('data' => array(0, 0, 0), 'class' => array('expected' => 3))
-        );
-        $testing[0] = array(
-            array('data' => array(1, 0, 1), 'class' => array('expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('expected' => 1)),
-            array('data' => array(0, 1, 0), 'class' => array('expected' => 3))
-        );
-        $testing[1] = array(
-            array('data' => array(1, 1, 1), 'class' => array('expected' => 1)),
-            array('data' => array(1, 0, 1), 'class' => array('expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('expected' => 1)),
-            array('data' => array(1, 1, 0), 'class' => array('expected' => 2)),
-            array('data' => array(0, 1, 0), 'class' => array('expected' => 2)),
-            array('data' => array(0, 0, 0), 'class' => array('expected' => 2)),
-            array('data' => array(0, 1, 0), 'class' => array('expected' => 3)),
-            array('data' => array(1, 1, 0), 'class' => array('expected' => 3)),
-            array('data' => array(0, 0, 0), 'class' => array('expected' => 3))
-        );
+            $training = array(
+                array('data' => array(1, 1, 1), 'class' => array('expected' => 1)),
+                array('data' => array(1, 0, 1), 'class' => array('expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('expected' => 2)),
+                array('data' => array(0, 1, 0), 'class' => array('expected' => 2)),
+                array('data' => array(0, 0, 0), 'class' => array('expected' => 2)),
+                array('data' => array(0, 1, 0), 'class' => array('expected' => 3)),
+                array('data' => array(1, 1, 0), 'class' => array('expected' => 3)),
+                array('data' => array(0, 0, 0), 'class' => array('expected' => 3))
+            );
+            $testing[0] = array(
+                array('data' => array(1, 0, 1), 'class' => array('expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('expected' => 1)),
+                array('data' => array(0, 1, 0), 'class' => array('expected' => 3))
+            );
+            $testing[1] = array(
+                array('data' => array(1, 1, 1), 'class' => array('expected' => 1)),
+                array('data' => array(1, 0, 1), 'class' => array('expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('expected' => 1)),
+                array('data' => array(1, 1, 0), 'class' => array('expected' => 2)),
+                array('data' => array(0, 1, 0), 'class' => array('expected' => 2)),
+                array('data' => array(0, 0, 0), 'class' => array('expected' => 2)),
+                array('data' => array(0, 1, 0), 'class' => array('expected' => 3)),
+                array('data' => array(1, 1, 0), 'class' => array('expected' => 3)),
+                array('data' => array(0, 0, 0), 'class' => array('expected' => 3))
+            );
 
-        $elm->registerMetadata($feature, $weight, $bias, $minMax, true, 'mape');
-        $elm->learn($training);
-        $elm->testForAccuracy($testing[0]);
-        print_r($testing[0]);
-        //print_r($elm->getData());
-    }
+            $elm->registerMetadata($feature, $weight, $bias, $minMax, true, 'mape');
+            $elm->learn($training);
+            $elm->testForAccuracy($testing[0]);
+            print_r($testing[0]);
+            //print_r($elm->getData());
+        }*/
 
     public function mftest($base = 'usd', $to = 'idr', $feature = 4, $total = 355)
     {
+        $isFlipped = false;
+        $dbCur = DBCurrency::getInstance();
+        $base = $dbCur->getCurrencyID($base);
+        $to = $dbCur->getCurrencyID($to);
+        if (($base != -1) && ($to != -1))
+        {
+            if ($base > $to)
+            {
+                $tmpVal = $base;
+                $base = $to;
+                $to = $tmpVal;
+                $isFlipped = true;
+                unset($tmpVal);
+            }
+            $dbExc = DBExchange::getInstance();
+            $dataTotal = $dbExc->getTotalData($base, $to);
+            $total = $dataTotal > $total ? $total : $dataTotal;
+            if (($total - $feature) > 1)
+            {
+                $data = $dbExc->getData($base, $to, $total, $isFlipped);
+                $training = $dbExc->formatData($data, $feature);
+                //print_r($training);
 
+                $elm = MElm::getInstance();
+                $weight = MElm::generateWeight($feature);
+                $bias = MElm::generateBias($feature);
+                $minMax = MElm::generateNormalizationBound($data, 100);
+                $elm->registerMetadata($feature, $weight, $bias, $minMax, true, 'mape');
+                $elm->learn($training);
+                //$elm->testForAccuracy($training);
+
+                $testing = array();
+                $sub_testing['data'] = array();
+                for ($cf = -1, $cs_f = $feature; ++$cf < $cs_f;)
+                {
+                    array_push($sub_testing['data'], $data[$cs_f - $cf - 1]);
+                }
+                array_push($testing, $sub_testing);
+
+
+                for ($ct = -1, $cs_t = 20; ++$ct < $cs_t;)
+                {
+                    $elm->test($testing);
+                    print_r($testing);
+                    foreach ($testing as $tk => $tkv)
+                    {
+                        for ($cf = -1, $cs_f = $feature - 1; ++$cf < $cs_f;)
+                        {
+                            $testing[$tk]['data'][$cf] = $testing[$tk]['data'][$cf + 1];
+                        }
+                        $testing[$tk]['data'][$feature - 1] = round($testing[$tk]['class']['actual'], 0);
+                    }
+                }
+            }
+            else
+            {
+                die("Insufficient data");
+            }
+        }
     }
 }
